0888bf211b7c047bf7652b8809ee12d44ee5a39a
Uzi Landsmann <uzi.landsmann@gmail.com>
2015-05-27 13:41:03 +0200

    Initial commit

---
+++ src/main/java/se/webstep/visumaat/Constants.java
@@ -0,0 +1,22 @@
+package se.webstep.visumaat;
+
+/**
+ * Class with constants
+ */
+public class Constants {
+
+    /**
+     * Path to the reports folder
+     */
+    public static String REPORTS_PATH = "reports";
+
+    /**
+     * Path to the VCS logfile
+     */
+    public static String LOGFILE_PATH = REPORTS_PATH + "/logfile.log";
+
+    /**
+     * Path to the configuration property file
+     */
+    public static String PROPERTIES_FILE_PATH = "config.txt";
+}
---
+++ src/main/java/se/webstep/visumaat/Visumaat.java
@@ -0,0 +1,56 @@
+package se.webstep.visumaat;
+
+import org.apache.commons.io.FileUtils;
+import se.webstep.visumaat.reports.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * A class which helps visualizing results of running the fabulous code-maat reports.
+ *
+ * @see <a href="https://github.com/adamtornhill/code-maat">https://github.com/adamtornhill/code-maat</a>
+ */
+public class Visumaat {
+
+    public static String maatPath;
+
+    /**
+     * Creates the VCS logfile and all the reports, and defines the REST paths to their JSON representations
+     */
+    public Visumaat() {
+
+        Properties props = readProperties();
+
+        VcsLog vcsLog = new VcsLog(props);
+        Summary summary = new Summary();
+        OrganizationalMetrics organizationalMetrics = new OrganizationalMetrics();
+        LogicalCoupling logicalCoupling = new LogicalCoupling();
+        CodeAge codeAge = new CodeAge();
+        AbsoluteChurn absoluteChurn = new AbsoluteChurn();
+        ChurnByAuthor churnByAuthor = new ChurnByAuthor();
+        ChurnByEntity churnByEntity = new ChurnByEntity();
+        EntityOwnership entityOwnership = new EntityOwnership();
+        EntityEffort entityEffort = new EntityEffort();
+    }
+
+    private Properties readProperties() {
+        Properties props = new Properties();
+        try {
+            props.load(FileUtils.openInputStream(new File(Constants.PROPERTIES_FILE_PATH)));
+
+            maatPath = props.getProperty("maat");
+            if (maatPath == null) {
+                throw new RuntimeException("Location of code_maat jar file was not defined in the configuration file (config.txt)");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return props;
+    }
+
+    public static void main(String[] args) {
+        new Visumaat();
+    }
+}
